443a64f9df0145566d5596622574fe7e5f1a6ecf
Martin <24-36840@g.batstate-u.edu.ph>
2025-11-18 11:31:14 +0800

    Group 2 created teacher table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db;USE school_db;
 CREATE TABLE student_table (student_id INT, student_name VARCHAR (50), grade_level INT);
+CREATE TABLE Teacher_Table(teacher_id INT, teacher_name VARCHAR (50), subject VARCHAR (50))
