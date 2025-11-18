7e378b2614e002208d989f7785c41d9a0ba82b19
Karl200515 <24-38720@g.batstate-u.edu.ph>
2025-11-18 11:51:04 +0800

    Group 4 added enrollment table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,8 @@ CREATE DATABASE school_db;USE school_db;
 CREATE TABLE student_table (student_id INT, student_name VARCHAR (50), grade_level INT);
 CREATE TABLE Teacher_Table(teacher_id INT, teacher_name VARCHAR (50), subject VARCHAR (50))
 CREATE TABLE Classes_Table(class_id INT, class_name VARCHAR(50), room_num INT)
+CREATE TABLE enrollment (
+    enrollment_id INT ,
+    student_id INT,
+    class_id INT
+);
